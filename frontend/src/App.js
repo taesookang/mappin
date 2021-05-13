e10b60be17e3457dd5815487996ffff9025e71fc
Taesoo Kang <tarrak123@gmail.com>
2021-05-13 02:13:16 -0400

    Frontend: Mapbox + Link to backend server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { Room, Star } from "@material-ui/icons";
+import "./App.css";
+import axios from "axios";
+import { format } from 'timeago.js'
 
 function App() {
+  const [pins, setPins] = useState([]);
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 46,
+    longitude: 17,
+    zoom: 4,
+  });
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      mapStyle="mapbox://styles/taesoo/ckomfhz8a36ed19qilwoawx7j"
+    >
+      {pins.map((p) => (
+        <>
+          <Marker
+            latitude={p.lat}
+            longitude={p.long}
+            offsetLeft={-20}
+            offsetTop={-10}
+            >
+            <Room style={{ fontSize: viewport.zoom * 7, color: "slateblue" }} />
+          </Marker>
+
+          <Popup
+          latitude={p.lat}
+          longitude={p.long}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left" >
+          <div className="card">
+            <label>Place</label>
+            <h4 className="place">{p.title}</h4>
+            <label>Review</label>
+            <p className="desc">{p.desc}</p>
+            <label>Rating</label>
+            <div className="stars">
+              <Star className="star"/>
+              <Star className="star"/>
+              <Star className="star"/>
+              <Star className="star"/>
+              <Star className="star"/>
+            </div>
+            <label>Information</label>
+            <span className="username">Created by <b>{p.username}</b></span>
+            <span className="date">{format(p.createdAt)}</span>
+            </div>
+        </Popup>
+        </>
+      ))}
+    </ReactMapGL>
   );
 }
 
