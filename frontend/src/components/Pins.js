a766f3731d5b9d21ee6d5db67dbe60f703f634b4
Taesoo Kang <tarrak123@gmail.com>
2021-05-17 01:39:33 -0400

    Minor warning fix

--- frontend/src/components/Pins.js
+++ frontend/src/components/Pins.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 import { Marker, Popup } from "react-map-gl";
 import { Room, Star } from "@material-ui/icons";
 import { format } from "timeago.js";
 import AddPin from "./AddPin";
-import "./Pins.css"
+import "./Pins.css";
 
 export default function Pins({
   currentUser,
@@ -35,53 +35,57 @@ export default function Pins({
       }
     };
     getPins();
-  });
+  }, []);
 
-  return pins.map((p) => (
+  return (
     <>
-      <Marker
-        latitude={p.lat}
-        longitude={p.long}
-        offsetLeft={-viewport.zoom * 3.5}
-        offsetTop={-viewport.zoom * 7}
-      >
-        <Room
-          style={{
-            fontSize: viewport.zoom * 7,
-            color: p.username === currentUser ? "salmon" : "slateblue",
-            cursor: "pointer",
-          }}
-          onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
-        />
-      </Marker>
-      {p._id === currentPlaceId && (
-        <Popup
-            key={p._id}
-          className="popup"
-          latitude={p.lat}
-          longitude={p.long}
-          closeButton={true}
-          closeOnClick={false}
-          anchor="left"
-          onClose={() => setCurrentPlaceId(null)}
-        >
-          <div className="card">
-            <label>Place</label>
-            <h4 className="place">{p.title}</h4>
-            <label>Review</label>
-            <p className="desc">{p.desc}</p>
-            <label>Rating</label>
-            <div className="stars">
-              {Array(parseInt(p.rating)).fill(<Star className="star" />)}
-            </div>
-            <label>Information</label>
-            <span className="username">
-              Created by <b>{p.username}</b>
-            </span>
-            <span className="date">{format(p.createdAt)}</span>
-          </div>
-        </Popup>
-      )}
+      {pins.map((p, idx) => (
+        <Fragment key={idx}>
+          <Marker
+            latitude={p.lat}
+            longitude={p.long}
+            offsetLeft={-viewport.zoom * 3.5}
+            offsetTop={-viewport.zoom * 7}
+          >
+            <Room
+              style={{
+                fontSize: viewport.zoom * 7,
+                color: p.username === currentUser ? "salmon" : "slateblue",
+                cursor: "pointer",
+              }}
+              onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
+            />
+          </Marker>
+          {p._id === currentPlaceId && (
+            <Popup
+              key={p._id}
+              className="popup"
+              latitude={p.lat}
+              longitude={p.long}
+              closeButton={true}
+              closeOnClick={false}
+              anchor="left"
+              onClose={() => setCurrentPlaceId(null)}
+            >
+              <div className="card">
+                <label>Place</label>
+                <h4 className="place">{p.title}</h4>
+                <label>Review</label>
+                <p className="desc">{p.desc}</p>
+                <label>Rating</label>
+                <div className="stars">
+                  {Array(parseInt(p.rating)).fill(<Star className="star" />)}
+                </div>
+                <label>Information</label>
+                <span className="username">
+                  Created by <b>{p.username}</b>
+                </span>
+                <span className="date">{format(p.createdAt)}</span>
+              </div>
+            </Popup>
+          )}
+        </Fragment>
+      ))}
       <AddPin
         newPlace={newPlace}
         setNewPlace={setNewPlace}
@@ -90,5 +94,5 @@ export default function Pins({
         currentUser={currentUser}
       />
     </>
-  ));
+  );
 }
